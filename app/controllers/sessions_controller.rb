e29b497f5ba0a36b79db36052fb47a241131088a
George Hall <ghall22@uncc.edu>
2017-05-08 01:27:11 +0000

    Fixed issue with sign in using downcase

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
   
   def create
-    account = Account.find_by(username: params[:session][:username].downcase)
+    account = Account.find_by(username: params[:session][:username])
     if account && account.authenticate(params[:session][:password])
       log_in(account)
       redirect_to account
     else
-      flash.now[:danger] = 'Invalid email/password combination. Please try again or create a new account.'
+      flash.now[:danger] = 'Invalid username/password combination. Please try again or create a new account.'
       render 'new'
     end
   end
